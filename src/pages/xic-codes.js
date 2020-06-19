eb97a77720c6a4bcea21a7d0b8514a224bb2ea43
Rob Graham <altabyte@gmail.com>
2020-06-19 13:16:08 +0100

    Display a list of the root category names.

--- src/pages/xic-codes.js
+++ src/pages/xic-codes.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {  } from '@material-ui/core';
+import _ from 'lodash';
+
+import XicData from '../data/xic-ids.json'
 
 export default function XicCodes() {
+
+    const roots = _.filter(XicData, (xic) => {
+        return _.isEmpty(_.get(xic, 'parent_ids', []));
+    });
+
     return (
         <div>
             <h1>
@@ -12,6 +20,18 @@ export default function XicCodes() {
             <Link to='/'>
                 Home page
             </Link>
+
+            <ul>
+                {
+                    _.map(roots, (root) => {
+                        return (
+                            <li>
+                                { _.get(root, 'name.en', 'UNKNOWN') }
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </div>
     );
 }
