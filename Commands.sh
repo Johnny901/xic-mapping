ba0853952f56bf303846035d8fef4fce28049460
Rob Graham <altabyte@gmail.com>
2020-06-19 12:34:50 +0100

    Add material-ui packages.

---
+++ Commands.sh
@@ -0,0 +1,11 @@
+# Shell commands used in developing this app.
+
+npx create-react-app xic-mapping-react
+
+yarn add react-router-dom
+yarn add axios
+yarn add @material-ui/core @material-ui/icons @material-ui/lab
+yarn add typeface-roboto
+yarn add lodash
+yarn add formik yup
+yarn add devour-client
