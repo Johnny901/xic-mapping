ff0ac0ae9952b4cf5ef464eed64a41d18434ccc2
Johnny <johnny@xpand.group>
2020-07-02 14:10:50 +0100

    Updates

---
+++ precache-manifest.10c406438a75a0c699f0b4d75ab523ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd4f23323cb6fe96133e624b8224a42",
+    "url": "/Johnny901/xic-mapping/index.html"
+  },
+  {
+    "revision": "aacef7fa8bdcabe8905b",
+    "url": "/Johnny901/xic-mapping/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6761cd1ecce73183eaba",
+    "url": "/Johnny901/xic-mapping/static/js/2.fa045bf5.chunk.js"
+  },
+  {
+    "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
+    "url": "/Johnny901/xic-mapping/static/js/2.fa045bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aacef7fa8bdcabe8905b",
+    "url": "/Johnny901/xic-mapping/static/js/main.e5ce52b6.chunk.js"
+  },
+  {
+    "revision": "44ccf415a08a21fd0bc9",
+    "url": "/Johnny901/xic-mapping/static/js/runtime-main.4e03a9c9.js"
+  }
+]);
