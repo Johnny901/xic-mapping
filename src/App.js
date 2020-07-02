4aad8b90419cf64f4064449ae2afcf2687602e0c
Johnny <johnny@xpand.group>
2020-07-02 16:05:29 +0100

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Routes from "./routes";
 function App() {
   return (
     <div>
-      <h1>XIC Mapping appjs</h1>
+      <h1>XIC Mapping appjs1</h1>
 
       <Router>
         <Link to="/xic-codes">XIC codes</Link>
